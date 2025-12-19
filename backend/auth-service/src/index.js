804c39cf7d4d771c042f7613f9406cea5bf3025d
alfredo0607 <alfredojosedominguezhernandez@gmail.com>
2025-12-19 16:45:00 -0500

    Refactor code structure for improved readability and maintainability

---
+++ backend/auth-service/src/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import http from "http";
+import cors from "cors";
+import morgan from "morgan";
+import fileUpload from "express-fileupload";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(fileUpload());
+
+server.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV === "development") {
+  server.use(morgan("dev"));
+}
+
+const serverHttps = http.createServer(server);
+
+server.get("/heart_check", (req, res) => {
+  return res.json({ message: "Servidor (AUTH) en linea." });
+});
+
+// server .use("/api/v1", router);
+
+serverHttps.listen(process.env.PORT, () => {
+  console.log(
+    `Servidor Express escuchando en el puerto (AUTH) ${process.env.PORT}`
+  );
+});
